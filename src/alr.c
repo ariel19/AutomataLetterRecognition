22d1b3ff5c18cb6b4f19ea1b936edfcebd7bdb3b
jablonskim <mjablonski92@gmail.com>
2014-10-18 16:35:53 +0200

    cleaning

--- src/alr.c
+++ src/alr.c
@@ -113,9 +113,6 @@ void automata_free(automata_t *atm) {
 }
 
 void automata_build(double *vec, automata_t *atm, msize_t input_size, feature_t *features, msize_t *err_num) {
-    *err_num = rand() % 1000 + 2000;
-    return;
-
 	atm_err_code ret;
 	fsize_t s;
 	mvec1_t out_vec,
--- src/swarm.c
+++ src/swarm.c
@@ -3,160 +3,160 @@
 #include <float.h>
 
 /* PSO */
-//double* pso_solve(unsigned int dim, unsigned int numParticles, double minX, double maxX,
-//              unsigned int maxEpochs, double exitError, double (*errorFunction)(double *x, int dim)) {
+double* pso_solve(unsigned int dim, unsigned int numParticles, double minX, double maxX,
+              unsigned int maxEpochs, double exitError, double (*errorFunction)(double *x, int dim)) {
 
 
-//    Particle* swarm = (Particle*)_malloc(numParticles * sizeof(Particle));
-//    double*   bestGlobalPosition = (double*)_malloc(dim * sizeof(double));
-//    double    bestGlobalError = DBL_MAX;
-//    double error;
-//    double lo, hi;
+    Particle* swarm = (Particle*)_malloc(numParticles * sizeof(Particle));
+    double*   bestGlobalPosition = (double*)_malloc(dim * sizeof(double));
+    double    bestGlobalError = DBL_MAX;
+    double error;
+    double lo, hi;
 
-//    unsigned int i, j, k; /* indicies */
-//    double* randomPosition;
-//    double* randomVelocity;
+    unsigned int i, j, k; /* indicies */
+    double* randomPosition;
+    double* randomVelocity;
 
-//    double w;         /* inertia weight */
-//    double c1;        /* cognitive weight */
-//    double c2;        /* social weight */
-//    double r1, r2;    /* cognitive and social randomizations */
-//    double probDeath;
-//    double die;
+    double w;         /* inertia weight */
+    double c1;        /* cognitive weight */
+    double c2;        /* social weight */
+    double r1, r2;    /* cognitive and social randomizations */
+    double probDeath;
+    double die;
 
-//    unsigned int epoch;
+    unsigned int epoch;
 
-//    double* newPosition;
-//    double* newVelocity;
-//    double  newError;
+    double* newPosition;
+    double* newVelocity;
+    double  newError;
 
-//    /* Initialization */
-//    for(i=0; i<numParticles; ++i){
-//        randomPosition = (double*)_malloc(dim *sizeof(double));
-//        randomVelocity = (double*)_malloc(dim *sizeof(double));
+    /* Initialization */
+    for(i=0; i<numParticles; ++i){
+        randomPosition = (double*)_malloc(dim *sizeof(double));
+        randomVelocity = (double*)_malloc(dim *sizeof(double));
 
-//        for(j=0; j< dim; ++j)
-//            randomPosition[j] = (maxX-minX)*( (double)rand()/RAND_MAX ) + minX;
+        for(j=0; j< dim; ++j)
+            randomPosition[j] = (maxX-minX)*( (double)rand()/RAND_MAX ) + minX;
 
-//        error = errorFunction(randomPosition, dim);
+        error = errorFunction(randomPosition, dim);
 
-//        for(j=0; j<dim;++j){
-//            lo = minX * 0.1;
-//            hi = maxX * 0.1;
-//            randomVelocity[j] =  (hi-lo)*( (double)rand()/RAND_MAX ) + lo;
-//        }
-//        swarm[i].position     = randomPosition;
-//        swarm[i].velocity     = randomVelocity;
-//        swarm[i].error        = error;
-//        swarm[i].bestPosition = randomPosition;
-//        swarm[i].bestError    = error;
+        for(j=0; j<dim;++j){
+            lo = minX * 0.1;
+            hi = maxX * 0.1;
+            randomVelocity[j] =  (hi-lo)*( (double)rand()/RAND_MAX ) + lo;
+        }
+        swarm[i].position     = randomPosition;
+        swarm[i].velocity     = randomVelocity;
+        swarm[i].error        = error;
+        swarm[i].bestPosition = randomPosition;
+        swarm[i].bestError    = error;
 
-//        if(swarm[i].error<bestGlobalError){
-//            bestGlobalError = swarm[i].error;
+        if(swarm[i].error<bestGlobalError){
+            bestGlobalError = swarm[i].error;
 
-//            /* Copy particles position to bestGlobalPosition */
-//            for(j=0;j<dim;++j){
-//                bestGlobalPosition[j] = swarm[i].position[j];
-//            }
+            /* Copy particles position to bestGlobalPosition */
+            for(j=0;j<dim;++j){
+                bestGlobalPosition[j] = swarm[i].position[j];
+            }
 
-//        }
-//    } /* Initialization */
+        }
+    } /* Initialization */
 
-//    w = 0.729;
-//    c1 = 1.49445;
-//    c2 = 1.49445;
-//    probDeath = 0.01;
+    w = 0.729;
+    c1 = 1.49445;
+    c2 = 1.49445;
+    probDeath = 0.01;
 
-//    epoch = 0;
+    epoch = 0;
 
-//    newPosition = (double*)_malloc(dim *sizeof(double));
-//    newVelocity = (double*)_malloc(dim *sizeof(double));
+    newPosition = (double*)_malloc(dim *sizeof(double));
+    newVelocity = (double*)_malloc(dim *sizeof(double));
 
-//    /* main loop */
-//    while(epoch<maxEpochs){
-//        for(i=0; i< numParticles; ++i){
+    /* main loop */
+    while(epoch<maxEpochs){
+        for(i=0; i< numParticles; ++i){
 
-//            Particle currP = swarm[i];
+            Particle currP = swarm[i];
 
-//            /* find new velocity */
-//            for(j = 0; j<numParticles; ++j){
-//                r1 = (double)rand()/RAND_MAX;
-//                r2 = (double)rand()/RAND_MAX;
+            /* find new velocity */
+            for(j = 0; j<numParticles; ++j){
+                r1 = (double)rand()/RAND_MAX;
+                r2 = (double)rand()/RAND_MAX;
 
-//                newVelocity[j] = (w * currP.velocity[j]) +
-//                                 (c1 * r1 * (currP.bestPosition[j] - currP.position[j])) +
-//                                 (c2 * r2 * (bestGlobalPosition[j] - currP.position[j]));
-//            }
-//            /* Change current particle velocity to newVelocity */
-//            for( k=0; k<dim;++k)
-//                currP.velocity[k] = newVelocity[k];
-//            /* find new position according to new velocity */
-//            for(j=0;j<dim;++j){
-//                newPosition[j] = currP.position[j] + newVelocity[j];
-//                if(newPosition[j] < minX)
-//                    newPosition[j] = minX;
-//                else if(newPosition[j] > maxX)
-//                    newPosition[j] = maxX;
-//            }
-//            /* set new position */
-//            for( k = 0; k<dim;++k)
-//                currP.position[k] = newPosition[k];
+                newVelocity[j] = (w * currP.velocity[j]) +
+                                 (c1 * r1 * (currP.bestPosition[j] - currP.position[j])) +
+                                 (c2 * r2 * (bestGlobalPosition[j] - currP.position[j]));
+            }
+            /* Change current particle velocity to newVelocity */
+            for( k=0; k<dim;++k)
+                currP.velocity[k] = newVelocity[k];
+            /* find new position according to new velocity */
+            for(j=0;j<dim;++j){
+                newPosition[j] = currP.position[j] + newVelocity[j];
+                if(newPosition[j] < minX)
+                    newPosition[j] = minX;
+                else if(newPosition[j] > maxX)
+                    newPosition[j] = maxX;
+            }
+            /* set new position */
+            for( k = 0; k<dim;++k)
+                currP.position[k] = newPosition[k];
 
-//            newError = errorFunction(newPosition, dim);
-//            currP.error = newError;
+            newError = errorFunction(newPosition, dim);
+            currP.error = newError;
 
-//            if (newError < currP.bestError){
-//                for( k=0; k<dim;++k)
-//                    currP.bestPosition[k] = newPosition[k];
-//                currP.bestError = newError;
-//            }
-//            /* set best global error */
-//            if (newError < bestGlobalError)
-//            {
-//                for( k=0; k<dim;++k)
-//                    bestGlobalPosition[k] = newPosition[k];
-//                bestGlobalError = newError;
-//            }
-//            /* Death */
-//            die = (double)rand()/RAND_MAX;
+            if (newError < currP.bestError){
+                for( k=0; k<dim;++k)
+                    currP.bestPosition[k] = newPosition[k];
+                currP.bestError = newError;
+            }
+            /* set best global error */
+            if (newError < bestGlobalError)
+            {
+                for( k=0; k<dim;++k)
+                    bestGlobalPosition[k] = newPosition[k];
+                bestGlobalError = newError;
+            }
+            /* Death */
+            die = (double)rand()/RAND_MAX;
 
-//            if (die < probDeath)
-//            {
-//                for (j = 0; j < dim; ++j)
-//                    currP.position[j] = (maxX-minX)*( (double)rand()/RAND_MAX ) + minX;
-//                currP.error = errorFunction(currP.position, dim);
+            if (die < probDeath)
+            {
+                for (j = 0; j < dim; ++j)
+                    currP.position[j] = (maxX-minX)*( (double)rand()/RAND_MAX ) + minX;
+                currP.error = errorFunction(currP.position, dim);
 
-//                for(k=0; k<dim; ++k)
-//                    currP.bestPosition[k]=currP.position[k];
+                for(k=0; k<dim; ++k)
+                    currP.bestPosition[k]=currP.position[k];
 
-//                currP.bestError = currP.error;
+                currP.bestError = currP.error;
 
-//                if (currP.error < bestGlobalError)
-//                {
-//                    bestGlobalError = currP.error;
-//                    for(k=0; k<dim; ++k)
-//                        bestGlobalPosition[k]=currP.position[k];
-//                }
-//            }
-//        } /* each particle */
-//        ++epoch;
-//    }
+                if (currP.error < bestGlobalError)
+                {
+                    bestGlobalError = currP.error;
+                    for(k=0; k<dim; ++k)
+                        bestGlobalPosition[k]=currP.position[k];
+                }
+            }
+        } /* each particle */
+        ++epoch;
+    }
 
-//    /* deinitialization */
-//    for(i=0;i<dim;++i){
-//        free(swarm[i].position);
-//        free(swarm[i].velocity);
-//        free(swarm[i].bestPosition);
+    /* deinitialization */
+    for(i=0;i<dim;++i){
+        free(swarm[i].position);
+        free(swarm[i].velocity);
+        free(swarm[i].bestPosition);
 
-//    }
-//    free(swarm);
-//    free(newPosition);
-//    free(newVelocity);
+    }
+    free(swarm);
+    free(newPosition);
+    free(newVelocity);
 
-//    return bestGlobalPosition;
-//}
+    return bestGlobalPosition;
+}
 
-void pso(unsigned int dim, unsigned int numParticles, double minX, double maxX, unsigned int maxEpochs, double exitError, 
+void pso(unsigned int dim, unsigned int numParticles, double minX, double maxX, unsigned int maxEpochs, double exitError,
          void (*errorFunction)(double *, automata_t *, msize_t, feature_t *, msize_t *),
          automata_t *atm, msize_t input_size, feature_t *features) {
 
@@ -184,8 +184,6 @@ void pso(unsigned int dim, unsigned int numParticles, double minX, double maxX, 
     double* newPosition;
     double* newVelocity;
     double  newError;
-
-    printf("PSO: before init\n");
 
     /* Initialization */
     for(i = 0; i < numParticles; ++i) {
@@ -223,18 +221,16 @@ void pso(unsigned int dim, unsigned int numParticles, double minX, double maxX, 
         swarm[i].bestPosition = randomPosition;
         swarm[i].bestError    = error;
 
-        if(swarm[i].error<bestGlobalError){
+        if(swarm[i].error<bestGlobalError) {
             bestGlobalError = swarm[i].error;
 
             /* Copy particles position to bestGlobalPosition */
-            for(j = 0;j < dim; ++j){
+            for(j = 0;j < dim; ++j) {
                 bestGlobalPosition[j] = swarm[i].position[j];
             }
 
         }
     } /* Initialization */
-
-    printf("PSO: after init\n");
 
     w = 0.729;
     c1 = 1.49445;
@@ -254,7 +250,7 @@ void pso(unsigned int dim, unsigned int numParticles, double minX, double maxX, 
             Particle currP = swarm[i];
 
             /* find new velocity */
-            for(j = 0; j < /*numParticles*/ dim; ++j){
+            for(j = 0; j < /*numParticles*/ dim; ++j) {
                 r1 = (double)rand()/RAND_MAX;
                 r2 = (double)rand()/RAND_MAX;
 
@@ -268,7 +264,7 @@ void pso(unsigned int dim, unsigned int numParticles, double minX, double maxX, 
                 currP.velocity[k] = newVelocity[k];
 
             /* find new position according to new velocity */
-            for(j = 0; j < dim; ++j){
+            for(j = 0; j < dim; ++j) {
                 newPosition[j] = currP.position[j] + newVelocity[j];
                 if(newPosition[j] < minX)
                     newPosition[j] = minX;
@@ -331,8 +327,6 @@ void pso(unsigned int dim, unsigned int numParticles, double minX, double maxX, 
 
         ++epoch;
     }
-
-    printf("PSO: after solve\n");
 
     /* deinitialization */
     for(i = 0; i < /* dim */numParticles; ++i){
